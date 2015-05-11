ec3800a0657bdbcc969a50b1e76346fbd34cd1dc
neolithos <neolithos@outlook.com>
2015-05-11 18:07:59 +0200

    Initial commit

---
+++ DESCore/Stuff/Objects.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo.IronLua;
+
+namespace TecWare.DES.Stuff
+{
+	///////////////////////////////////////////////////////////////////////////////
+	/// <summary></summary>
+	public static partial class Procs
+	{
+		#region -- FreeAndNil -------------------------------------------------------------
+
+		/// <summary>Ruft IDisposable.Dispose, wenn es implementiert wurde. Sonst wird die Variable auf <c>null</c> gesetzt.</summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="obj"></param>
+		public static void FreeAndNil<T>(ref T obj)
+			where T : class
+		{
+			// Optionaler Call von Dispose
+			IDisposable disp = obj as IDisposable;
+			if (disp != null)
+				disp.Dispose();
+
+			// Setze die Referenz auf Null
+			obj = null;
+		} // proc FreeAndNil
+
+		#endregion
+
+		#region -- ChangeType -------------------------------------------------------------
+
+		/// <summary>Konvertiert den Datentyp zum gewünschten Ziel-Datentyp.</summary>
+		/// <param name="value"></param>
+		/// <param name="typeTo"></param>
+		/// <returns></returns>
+		/// <remarks>Es wird der Lua Converter verwendet, da er Schnittstelle, Operatoren und allgemeine Regeln beherrscht ohne auf das TypeDescriptor-Framework zu verweisen.</remarks>
+		public static object ChangeType(object value, Type typeTo)
+		{
+			return Lua.RtConvertValue(value, typeTo);
+		} // func ChangeType
+
+		/// <summary>Generische Implementierung von <c>ChangeType</c>.</summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static T ChangeValue<T>(object value)
+		{
+			return (T)Procs.ChangeType(value, typeof(T));
+		} // func ChangeValue
+
+		// Todo: Dynamische Implementierung
+		//       Erzeugt eine CallSite, die in einer Variable gecached werden muss, und immer wieder gerufen wird.
+
+		#endregion
+
+	} // class Procs
+}
