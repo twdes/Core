59e2dcc193264067cda0f6c993eb98964884ccf8
neolithos <neolithos@outlook.com>
2015-08-13 13:49:23 +0200

    Xml-Helper, WebRequest-Helper

---
+++ DESCore/Stuff/Uri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecWare.DES.Stuff
+{
+	public static partial class UriHelper
+	{
+		public static string GetFileName(this Uri uri)
+		{
+			return Path.GetFileName(uri.AbsolutePath);
+		} // func GetFileName
+	} // class Procs
+}
