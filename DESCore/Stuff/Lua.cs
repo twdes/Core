9a839187d82480814f184f750d938ce35982d5d1
neolithos <neolithos@outlook.com>
2015-09-01 19:51:46 +0200

    In c# 6 there is a better solution for CreateLuaTable

--- DESCore/Stuff/Lua.cs
+++ DESCore/Stuff/Lua.cs
@@ -9,14 +9,5 @@ namespace TecWare.DES.Stuff
 {
 	public static partial class Procs
 	{
-		public static LuaTable CreateLuaTable(params KeyValuePair<string, object>[] args)
-		{
-			var t = new LuaTable();
-
-			foreach (var c in args)
-				t[c.Key] = c.Value;
-
-			return t;
-		} // func CreateLuaTable
 	} // class Procs
 }
