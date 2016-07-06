db4933e6cc14e303d410ff51ae89f8760db94d83
neolithos <neolithos@outlook.com>
2016-07-06 12:12:00 +0200

    Add: Missing file Strings.cs

---
+++ DESCore/Stuff/Strings.cs
@@ -0,0 +1,36 @@
+﻿#region -- copyright --
+//
+// Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+// European Commission - subsequent versions of the EUPL(the "Licence"); You may
+// not use this work except in compliance with the Licence.
+//
+// You may obtain a copy of the Licence at:
+// http://ec.europa.eu/idabc/eupl
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+// specific language governing permissions and limitations under the Licence.
+//
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecWare.DE.Stuff
+{
+	public static partial class Procs
+	{
+		public static string EscapeSpecialChars(this string value)
+		{
+			return value; // todo: escape \n \r \t
+		} // proc EscapeSpecialChars
+
+		public static string UnescapeSpecialChars(this string value)
+		{
+			return value; // todo:
+		} // func UnescapeSpecialChars
+	} // class Procs
+}
