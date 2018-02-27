85b1098d596130bffe6cc7ef7124d0a4533b47c5
neolithos <neolithos@outlook.com>
2018-02-27 17:29:58 +0100

    Chg: TextDataRowEnumerator uses IDataRow

--- DESCore/Data/CsvReaderDynamic.cs
+++ DESCore/Data/CsvReaderDynamic.cs
@@ -18,167 +18,147 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TecWare.DE.Stuff;
 
 namespace TecWare.DE.Data
 {
-	#region -- class TextDynamicColumn --------------------------------------------------
+	#region -- class TextDataRowColumn ------------------------------------------------
 
-	///////////////////////////////////////////////////////////////////////////////
 	/// <summary>Column description</summary>
-	public sealed class TextDynamicColumn
+	public sealed class TextDataRowColumn : IDataColumn
 	{
 		/// <summary>Name of the column</summary>
-		public string ColumnName { get; set; }
+		public string Name { get; set; }
 		/// <summary>Data type</summary>
 		public Type DataType { get; set; }
 		/// <summary>Optional special provider for the format of the value.</summary>
 		public IFormatProvider FormatProvider { get; set; }
 		/// <summary>Convert Type.</summary>
 		public Func<string, object> Converter { get; set; }
-	} // class TextDynamicColumn
+		/// <summary>Empty attributes</summary>
+		public IPropertyEnumerableDictionary Attributes => PropertyDictionary.EmptyReadOnly;
+	} // class TextDataRowColumn
 
 	#endregion
 
-	#region -- class TextDynamicRow -----------------------------------------------------
+	#region -- class TextDataRowEnumerator --------------------------------------------
 
-	///////////////////////////////////////////////////////////////////////////////
 	/// <summary></summary>
-	public sealed class TextDynamicRow
+	public sealed class TextDataRowEnumerator : TextRowEnumerator<IDataRow>
 	{
-		private TextDynamicRowEnumerator owner;
-		private int version = 0;
+		#region -- class TextDataRow --------------------------------------------------
 
-		internal TextDynamicRow(TextDynamicRowEnumerator owner)
+		private sealed class TextDataRow : DynamicDataRow
 		{
-			this.owner = owner;
-		} // ctor
+			private readonly TextDataRowEnumerator enumerator;
 
-		internal void Reset()
-		{
-			version++;
-		} // proc Reset
-
-		private object GetValueIntern(int columnIndex)
-		{
-			// get value
-			var value = owner.CoreReader[columnIndex];
-			var column = owner.Columns?[columnIndex];
-			if (column == null)
-				return null; // no column defined
-
-			// convert
-			if (column.Converter != null)
-				return column.Converter(value);
-			else if (column.DataType == typeof(decimal))
-				return Decimal.Parse(value, NumberStyles.Currency | NumberStyles.Float, column.FormatProvider);
-			else if (column.DataType == typeof(double))
-				return Double.Parse(value, NumberStyles.Currency | NumberStyles.Float, column.FormatProvider);
-			else if (column.DataType == typeof(float))
-				return Single.Parse(value, NumberStyles.Currency | NumberStyles.Float, column.FormatProvider);
-
-			else if (column.DataType == typeof(byte))
-				return Byte.Parse(value, NumberStyles.Integer, column.FormatProvider);
-			else if (column.DataType == typeof(sbyte))
-				return SByte.Parse(value, NumberStyles.Integer, column.FormatProvider);
-			else if (column.DataType == typeof(ushort))
-				return UInt16.Parse(value, NumberStyles.Integer, column.FormatProvider);
-			else if (column.DataType == typeof(short))
-				return Int16.Parse(value, NumberStyles.Integer, column.FormatProvider);
-			else if (column.DataType == typeof(uint))
-				return UInt32.Parse(value, NumberStyles.Integer, column.FormatProvider);
-			else if (column.DataType == typeof(int))
-				return Int32.Parse(value, NumberStyles.Integer, column.FormatProvider);
-			else if (column.DataType == typeof(ulong))
-				return UInt64.Parse(value, NumberStyles.Integer, column.FormatProvider);
-			else if (column.DataType == typeof(long))
-				return Int64.Parse(value, NumberStyles.Integer, column.FormatProvider);
-
-			else if (column.DataType == typeof(DateTime))
-				return DateTime.Parse(value, column.FormatProvider, DateTimeStyles.AssumeLocal);
-
-			else if (column.DataType == typeof(string))
-				return value;
-
-			else
-				return Procs.ChangeType(value, column.DataType);
-		} // func GetValue
-
-		public object GetValue(int columnIndex)
-		{
-			try
+			public TextDataRow(TextDataRowEnumerator enumerator)
 			{
-				return GetValueIntern(columnIndex);
-			}
-			catch (Exception e)
-			{
-				Debug.WriteLine(String.Format("[{0}] {1}", e.GetType().Name, e.Message));
-					if (owner.IsParsedStrict)
-						throw;
-					else
-						return null;
-			}
-		} // func GetValue
+				this.enumerator = enumerator ?? throw new ArgumentNullException(nameof(enumerator));
+			} // ctor
 
-		public T GetValue<T>(int columnIndex)
-		{
-			try
+			private object GetValueIntern(int index)
 			{
-				var obj = GetValueIntern(columnIndex);
-				return obj == null ? default(T) :(T)obj;
-			}
-			catch (Exception e)
-			{
-				Debug.WriteLine(String.Format("[{0}] {1}", e.GetType().Name, e.Message));
-				if (owner.IsParsedStrict)
-					throw;
+				// get value
+				var value = enumerator.CoreReader[index];
+				var column = enumerator.Columns?[index];
+				if (column == null)
+					return null; // no column defined
+
+				// convert
+				if (column.Converter != null)
+					return column.Converter(value);
+				else if (column.DataType == typeof(decimal))
+					return Decimal.Parse(value, NumberStyles.Currency | NumberStyles.Float, column.FormatProvider);
+				else if (column.DataType == typeof(double))
+					return Double.Parse(value, NumberStyles.Currency | NumberStyles.Float, column.FormatProvider);
+				else if (column.DataType == typeof(float))
+					return Single.Parse(value, NumberStyles.Currency | NumberStyles.Float, column.FormatProvider);
+
+				else if (column.DataType == typeof(byte))
+					return Byte.Parse(value, NumberStyles.Integer, column.FormatProvider);
+				else if (column.DataType == typeof(sbyte))
+					return SByte.Parse(value, NumberStyles.Integer, column.FormatProvider);
+				else if (column.DataType == typeof(ushort))
+					return UInt16.Parse(value, NumberStyles.Integer, column.FormatProvider);
+				else if (column.DataType == typeof(short))
+					return Int16.Parse(value, NumberStyles.Integer, column.FormatProvider);
+				else if (column.DataType == typeof(uint))
+					return UInt32.Parse(value, NumberStyles.Integer, column.FormatProvider);
+				else if (column.DataType == typeof(int))
+					return Int32.Parse(value, NumberStyles.Integer, column.FormatProvider);
+				else if (column.DataType == typeof(ulong))
+					return UInt64.Parse(value, NumberStyles.Integer, column.FormatProvider);
+				else if (column.DataType == typeof(long))
+					return Int64.Parse(value, NumberStyles.Integer, column.FormatProvider);
+
+				else if (column.DataType == typeof(DateTime))
+					return DateTime.Parse(value, column.FormatProvider, DateTimeStyles.AssumeLocal);
+
+				else if (column.DataType == typeof(string))
+					return value;
+
 				else
-					return default(T);
-			}
-		} // func GetValue
+					return Procs.ChangeType(value, column.DataType);
+			} // func GetValueIntern
 
-		public T GetValue<T>(string columnName)
-			=> GetValue<T>(IndexOfColumn(columnName));
+			public override object this[int index]
+			{
+				get
+				{
+					try
+					{
+						return GetValueIntern(index);
+					}
+					catch (Exception e)
+					{
+						Debug.WriteLine(String.Format("[{0}] {1}", e.GetType().Name, e.Message));
+						if (enumerator.IsParsedStrict)
+							throw;
+						else
+							return null;
+					}
+				}
+			} // func this
 
-		public int IndexOfColumn(string columnName)
-			=> Array.FindIndex(owner.Columns, c => String.Compare(c.ColumnName, columnName, StringComparison.OrdinalIgnoreCase) == 0);
+			public override bool IsDataOwner => false;
 
-		public object this[int columnIndex] => GetValue(columnIndex);
-		public object this[string columnName] => GetValue(IndexOfColumn(columnName));
-	} // class TextDynamicRow
+			public override IReadOnlyList<IDataColumn> Columns => enumerator.Columns;
+		} // class TextDataRow
 
-	#endregion
+		#endregion
 
-	#region -- class TextDynamicRowEnumerator -------------------------------------------
-
-	///////////////////////////////////////////////////////////////////////////////
-	/// <summary></summary>
-	public sealed class TextDynamicRowEnumerator : TextRowEnumerator<TextDynamicRow>
-	{
-		private TextDynamicColumn[] columns = null;
-		private readonly TextDynamicRow currentRow;
+		private TextDataRowColumn[] columns = null;
+		private readonly TextDataRow currentRow;
 
 		#region -- Ctor/Dtor --------------------------------------------------------------
 
-		public TextDynamicRowEnumerator(ITextCoreReader coreReader)
+		/// <summary></summary>
+		/// <param name="coreReader"></param>
+		public TextDataRowEnumerator(ITextCoreReader coreReader)
 			: base(coreReader)
 		{
-			this.currentRow = new TextDynamicRow(this);
+			this.currentRow = new TextDataRow(this);
 		} // ctor
 
-		public TextDynamicRowEnumerator(TextReader tr, TextCsvSettings settings)
+		/// <summary></summary>
+		/// <param name="tr"></param>
+		/// <param name="settings"></param>
+		public TextDataRowEnumerator(TextReader tr, TextCsvSettings settings)
 			: this(new TextCsvReader(tr, settings))
 		{
 		} // ctor
 
-		public TextDynamicRowEnumerator(TextReader tr, TextFixedSettings settings)
+		/// <summary></summary>
+		/// <param name="tr"></param>
+		/// <param name="settings"></param>
+		public TextDataRowEnumerator(TextReader tr, TextFixedSettings settings)
 			: this(new TextFixedReader(tr, settings))
 		{
 		} // ctor
 
+		/// <summary></summary>
+		/// <param name="disposing"></param>
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing)
@@ -191,16 +171,15 @@ namespace TecWare.DE.Data
 
 		/// <summary>Sets the column description.</summary>
 		/// <param name="columns"></param>
-		public void UpdateColumns(params TextDynamicColumn[] columns)
+		public void UpdateColumns(params TextDataRowColumn[] columns)
 		{
 			this.columns = columns;
-			currentRow.Reset();
 		} // proc UpdateColumns
 
 		/// <summary>The returned reference is reused.</summary>
-		public override TextDynamicRow Current => currentRow;
+		public override IDataRow Current => currentRow;
 		/// <summary>Column definition.</summary>
-		public TextDynamicColumn[] Columns => columns;
+		public TextDataRowColumn[] Columns => columns;
 		/// <summary></summary>
 		public bool IsParsedStrict { get; set; } = false;
 	} // class TextDynamicRowEnumerator
