e0fba3ca250735c018592f73c35a149c76f0a439
neolithos <neolithos@outlook.com>
2019-10-09 17:32:27 +0200

    Add: LazyProperty pattern

--- DESCore/Stuff/Objects.cs
+++ DESCore/Stuff/Objects.cs
@@ -43,6 +43,141 @@ namespace TecWare.DE.Stuff
 
 	#endregion
 
+	#region -- class LazyProperty -----------------------------------------------------
+
+	/// <summary>Property implementation for Lazy via Binding or Task..</summary>
+	/// <typeparam name="T"></typeparam>
+	public sealed class LazyProperty<T>
+	{
+		#region -- enum LazyPropertyState -----------------------------------------------
+
+		private enum LazyPropertyState
+		{
+			Nothing,
+			Loading,
+			Loaded
+		} // enum LazyPropertyState
+
+		#endregion
+
+		private readonly Func<Task<T>> getValue;
+		private readonly Action onPropertyChanged;
+
+		private T value;
+		private LazyPropertyState state;
+
+		private readonly List<TaskCompletionSource<T>> valueListener = new List<TaskCompletionSource<T>>();
+
+		/// <summary></summary>
+		/// <param name="getValue"></param>
+		/// <param name="onPropertyChanged"></param>
+		public LazyProperty(Func<Task<T>> getValue, Action onPropertyChanged)
+		{
+			this.getValue = getValue;
+			this.onPropertyChanged = onPropertyChanged;
+
+			Reset();
+		} // ctor
+
+		/// <summary>Clear value of the property.</summary>
+		public void Reset()
+		{
+			state = LazyPropertyState.Nothing;
+			value = default;
+
+			onPropertyChanged?.Invoke();
+		} // proc Reset
+
+		private async Task<T> GetDataInternalAsync()
+		{
+			try
+			{
+				return SetValueIntern(await getValue(), null);
+			}
+			catch (Exception e)
+			{
+				SetValueIntern(default, e);
+				throw;
+			}
+		} // func GetDataInternalAsync
+
+		private T SetValueIntern(T t, Exception e)
+		{
+			lock (getValue)
+			{
+				value = t;
+				state = LazyPropertyState.Loaded;
+
+				foreach (var c in valueListener)
+				{
+					if (e == null)
+						c.SetResult(t);
+					else
+						c.SetException(e);
+				}
+				valueListener.Clear();
+			}
+			onPropertyChanged?.Invoke();
+
+			return value;
+		} // func SetValueIntern
+
+		/// <summary>Get the value in async mode.</summary>
+		/// <returns></returns>
+		public Task<T> GetValueAsync()
+		{
+			lock (getValue)
+			{
+				switch (state)
+				{
+					case LazyPropertyState.Nothing:
+						state = LazyPropertyState.Loading;
+						return GetDataInternalAsync();
+					case LazyPropertyState.Loading:
+						var t = new TaskCompletionSource<T>();
+						valueListener.Add(t);
+						return t.Task;
+					case LazyPropertyState.Loaded:
+						return Task.FromResult(value);
+					default:
+						throw new InvalidOperationException();
+				}
+			}
+		} // func GetValueAsync
+
+		/// <summary>Get current value, or start loading of the value.</summary>
+		/// <returns></returns>
+		public T GetValue()
+		{
+			lock (getValue)
+			{
+				switch (state)
+				{
+					case LazyPropertyState.Nothing:
+						state = LazyPropertyState.Loading;
+						getValue().ContinueWith(
+							t =>
+							{
+								if (t.IsFaulted)
+									SetValueIntern(default, t.Exception);
+								else
+									SetValueIntern(t.Result, null);
+							}, TaskContinuationOptions.ExecuteSynchronously
+						);
+						return state == LazyPropertyState.Loaded ? value : default;
+					case LazyPropertyState.Loading:
+						return default;
+					case LazyPropertyState.Loaded:
+						return value;
+					default:
+						throw new InvalidOperationException();
+				}
+			}
+		} // func GetValue
+	} // class LazyProperty
+
+	#endregion
+
 	/// <summary></summary>
 	public static partial class Procs
 	{
